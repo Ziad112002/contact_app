167597669bf42c3842b8953c7e95a599afd7e714
ziad <mqpz596@gmail.com>
2025-12-23 02:28:22 +0200

    finished app

--- lib/styles/app_assets.dart
+++ lib/styles/app_assets.dart
@@ -1,8 +1,5 @@
 abstract final class AppAssets {
   static const String routeLogo = "assets/images/logo.png";
-  static const String kilyanPhoto = "assets/images/kilyan.jpeg";
-  static const String moPhoto = "assets/images/moface.jpeg";
-  static const String ronaldoPhoto = "assets/images/ronaldo face .jpeg";
   static const String emptyListAnimation = "assets/animations/Empty bookings.json";
   static const String imagePickerAnimation = "assets/animations/image_picker.json";
 }
